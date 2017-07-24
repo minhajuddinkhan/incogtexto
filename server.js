784f66d487ca09c8ad09ba0cc28384ceff05e05e
Minhaj Uddin Khan <minhajuddinkhan75@gmail.com>
2017-07-24 18:13:07 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const interfaces = require('os').networkInterfaces()["eth0"];
 const QrCode = require('qrcode');
+const app = require('express')();
+
+
 const ipv4 = interfaces.find((interface) => interface.family === 'IPv4');
 const ipv4str = JSON.stringify(ipv4);
-const app = require('express')();
+
 
 app.get('/', (req, res) => {
   QrCode.toDataURL(ipv4str, (err, url) =>  {
