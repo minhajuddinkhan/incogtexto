e739a1a0aafb31a2a831d8633b1d5c7b4acad8b7
Minhaj <minhajuddinkhan75@gmail.com>
2017-07-24 23:19:16 +0500

    testing network

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const interfaces = require('os').networkInterfaces()["eth0"];
 const QrCode = require('qrcode');
 const app = require('express')();
-
+const network = require('network');
 
 const ipv4 = interfaces.find((interface) => interface.family === 'IPv4');
 const ipv4str = JSON.stringify(ipv4);
 
+
+network.get_interfaces_list(function(err, list) {
+  console.log(list)
+})
 
 app.get('/', (req, res) => {
   QrCode.toDataURL(ipv4str, (err, url) =>  {
